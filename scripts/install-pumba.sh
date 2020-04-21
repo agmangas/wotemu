1827eda442cb41bb862023129853275220943b92
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-04-21 18:17:48 +0200

    Update Pumba to 0.7.2

--- scripts/install-pumba.sh
+++ scripts/install-pumba.sh
@@ -3,6 +3,6 @@
 set -e
 
 cd /usr/bin
-wget --quiet https://github.com/alexei-led/pumba/releases/download/0.6.8/pumba_linux_amd64
+wget --quiet https://github.com/alexei-led/pumba/releases/download/0.7.2/pumba_linux_amd64
 mv pumba_linux_amd64 pumba
 chmod +x pumba
