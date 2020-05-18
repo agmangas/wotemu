05cd12d642f14b26d1c9aa4dfc757716757202af
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-05-18 20:02:26 +0200

    Create topology subpackage

--- wotsim/enums.py
+++ wotsim/enums.py
@@ -4,3 +4,16 @@ import enum
 class Labels(enum.Enum):
     WOTSIM_NETWORK = "org.fundacionctic.wotsim.net"
     WOTSIM_GATEWAY = "org.fundacionctic.wotsim.gw"
+
+
+class NetworkConditions(enum.Enum):
+    THREEG = "3G"
+    WIFI = "WIFI"
+    CABLE = "CABLE"
+
+
+class NodePlatforms(enum.Enum):
+    CONSTRAINED = "CONSTRAINED"
+    GATEWAY = "LOW_TIER"
+    CLOUD = "CLOUD"
+    UNCONSTRAINED = "UNCONSTRAINED"
