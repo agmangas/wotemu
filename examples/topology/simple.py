0650b1e8e3d6149ee0cb49ae74f632496883db54
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-06-01 18:34:29 +0200

    Update topology example

--- examples/topology/simple.py
+++ examples/topology/simple.py
@@ -1,6 +1,8 @@
 import sys
 
+from wotsim.enums import NetworkConditions
 from wotsim.topology.models import Broker, Network, Node, NodeApp, Topology
+from wotsim.topology.utils import conditions_to_netem
 
 APP_CLOCK = "/root/wotsim/examples/app/clock.py"
 APP_SUBSCRIBER = "/root/wotsim/examples/app/subscriber.py"
@@ -10,58 +12,43 @@ THING_ID_CLOCK = "urn:org:fundacionctic:thing:clock"
 
 
 def build_topology():
-    network_cable = Network(
-        name="cable",
-        netem=["delay --time 5 --jitter 1 --distribution normal"])
-
     network_wifi = Network(
         name="wifi",
-        netem=["delay --time 15 --jitter 10 --distribution normal"])
+        netem=conditions_to_netem(NetworkConditions.WIFI))
 
     network_3g = Network(
         name="mobile_3g",
-        netem=[
-            "delay --time 100 --jitter 50 --distribution normal",
-            "rate --rate 200kbit"
-        ])
+        netem=conditions_to_netem(NetworkConditions.THREEG))
 
-    broker = Broker(name="broker", networks=[network_3g])
+    broker = Broker(
+        name="broker",
+        networks=[network_3g])
 
-    node_cable = Node(
-        name="node_cable",
-        app=NodeApp(path=APP_CLOCK, ws=True),
-        networks=[network_cable])
-
-    node_wifi_cable = Node(
-        name="node_wifi_cable",
-        app=NodeApp(path=APP_CLOCK, mqtt=True),
-        networks=[network_wifi, network_cable, network_3g],
+    node_clock = Node(
+        name="clock",
+        app=NodeApp(path=APP_CLOCK, http=True, mqtt=True),
+        networks=[network_wifi, network_3g],
         broker=broker,
         broker_network=network_3g)
 
-    node_wifi_1 = Node(
-        name="node_wifi_1",
-        app=NodeApp(path=APP_CLOCK, http=True, coap=True),
-        networks=[network_wifi])
-
-    node_wifi_cable_host = "{}.{}".format(
-        node_wifi_cable.name,
+    host_clock = "{}.{}".format(
+        node_clock.name,
         network_wifi.name)
 
-    app_wifi_2 = NodeApp(
+    app_sub = NodeApp(
         path=APP_SUBSCRIBER,
         params={
-            PARAM_SERVIENT_HOST: node_wifi_cable_host,
+            PARAM_SERVIENT_HOST: host_clock,
             PARAM_THING_ID: THING_ID_CLOCK
         })
 
-    node_wifi_2 = Node(
-        name="node_wifi_2",
-        app=app_wifi_2,
-        networks=[network_wifi, network_3g])
+    node_sub = Node(
+        name="clock_sub",
+        app=app_sub,
+        networks=[network_wifi, network_3g],
+        scale=3)
 
-    topology = Topology(
-        nodes=[node_wifi_1, node_wifi_2, node_cable, node_wifi_cable])
+    topology = Topology(nodes=[node_clock, node_sub])
 
     return topology
 
