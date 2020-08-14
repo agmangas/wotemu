37d59a774001d0f5abe7c17d055141b6aecf7881
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-08-14 13:57:45 +0200

    Apply nest if available

--- wotemu/__init__.py
+++ wotemu/__init__.py
@@ -1,4 +1,11 @@
 import logging
 
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
+_logger = logging.getLogger(__name__)
+_logger.addHandler(logging.NullHandler())
+
+try:
+    import nest_asyncio
+    nest_asyncio.apply()
+    _logger.info("Patched loop with: %s", nest_asyncio)
+except ImportError:
+    _logger.info("Using default asyncio loop")
