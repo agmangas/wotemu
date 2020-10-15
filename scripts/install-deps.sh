89276ec61f26909759e7b904a0043b57b0324c62
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-10-15 16:24:00 +0200

    Add commands to install RVM

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,4 +3,16 @@
 set -e
 
 apt-get update -y
-apt-get install -y sshpass jq python3-pip
+
+apt-get install -y \
+sshpass \
+jq \
+python3-pip
+
+# RVM
+
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys \
+409B6B1796C275462A1703113804BB82D39DC0E3 \
+7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+curl -sSL https://get.rvm.io | bash -s stable --rails
