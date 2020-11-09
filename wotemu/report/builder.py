3263a21f160963b618d0a56148d8a27a583da71f
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-11-09 17:32:55 +0100

    Add CPU figures and update layout

--- wotemu/report/builder.py
+++ wotemu/report/builder.py
@@ -1,4 +1,5 @@
 import io
+import math
 import xml.etree.ElementTree as ET
 
 import pandas as pd
@@ -10,21 +11,21 @@ from plotly.subplots import make_subplots
 
 class ReportBuilder:
     @classmethod
-    def build_figure_block_element(cls, fig, title=None, height=450, col_class="col-md", with_row=True):
+    def build_figure_block_el(cls, fig, title=None, height=450, col_class="col", with_row=True):
         try:
             fig_io = io.StringIO()
             fig.write_html(fig_io, include_plotlyjs=False, full_html=False)
             fig_html = fig_io.getvalue()
 
-            el_title = ET.Element("h4")
-
-            if title:
-                el_title.text = title
-
             el_plot = ET.fromstring(fig_html)
             el_plot.set("style", f"height: {height}px;")
             el_col = ET.Element("div", attrib={"class": col_class})
-            el_col.append(el_title)
+
+            if title:
+                el_title = ET.Element("h4")
+                el_title.text = title
+                el_col.append(el_title)
+
             el_col.append(el_plot)
 
             if not with_row:
@@ -39,6 +40,31 @@ class ReportBuilder:
                 fig_io.close()
             except:
                 pass
+
+    @classmethod
+    def build_figures_row_el(cls, figs, title=None, height=450, col_class="col-xl"):
+        fig_elements = [
+            cls.build_figure_block_el(
+                fig,
+                title=None,
+                height=height,
+                col_class=col_class,
+                with_row=False)
+            for fig in figs
+        ]
+
+        el_row = ET.Element("div", attrib={"class": "row"})
+
+        if title:
+            el_title = ET.Element("h4")
+            el_title.text = title
+            el_title_col = ET.Element("div", attrib={"class": "col-xl-12"})
+            el_title_col.append(el_title)
+            el_row.append(el_title_col)
+
+        [el_row.append(item) for item in fig_elements]
+
+        return el_row
 
     def __init__(self, reader):
         self._reader = reader
@@ -71,11 +97,55 @@ class ReportBuilder:
 
         return fig
 
+    async def build_task_cpu_figure(self, task):
+        df_system = await self._reader.get_system_df(task=task)
+        df_system = df_system.reset_index()
+
+        has_secondary = "cpu_percent_constraint" in df_system \
+            and df_system["cpu_percent_constraint"].any()
+
+        fig = make_subplots(specs=[[{"secondary_y": has_secondary}]])
+
+        trace_primary = go.Scatter(
+            x=df_system["date"],
+            y=df_system["cpu_percent"],
+            name="CPU total (%)")
+
+        fig.add_trace(trace_primary, secondary_y=False)
+
+        fig.update_layout(title_text="CPU usage")
+        fig.update_xaxes(title_text="Date (UTC)")
+
+        fig.update_yaxes(
+            title_text="% over total",
+            secondary_y=False)
+
+        if has_secondary:
+            trace_secondary = go.Scatter(
+                x=df_system["date"],
+                y=df_system["cpu_percent_constraint"],
+                name="CPU constraint (%)",
+                line=dict(dash="dot"))
+
+            fig.add_trace(trace_secondary, secondary_y=True)
+
+            fig.update_yaxes(
+                title_text="% over constraint",
+                secondary_y=True,
+                range=[0.0, 100.0])
+
+        return fig
+
     async def build_report(self, plot_height=500):
         tasks = await self._reader.get_tasks()
 
-        figs = {
+        figs_mem = {
             task: await self.build_task_mem_figure(task=task)
+            for task in tasks
+        }
+
+        figs_cpu = {
+            task: await self.build_task_cpu_figure(task=task)
             for task in tasks
         }
 
@@ -87,7 +157,12 @@ class ReportBuilder:
             el for el in tree.find("body").iter()
             if el.attrib.get("id") == "root")
 
-        for task, fig in figs.items():
-            root.append(self.build_figure_block_element(fig, title=task))
+        for task in sorted(figs_mem.keys()):
+            task_row = self.build_figures_row_el(
+                [figs_mem[task], figs_cpu[task]],
+                col_class="col-xl-6",
+                title=task)
+
+            root.append(task_row)
 
         return ET.tostring(tree, method="html")
