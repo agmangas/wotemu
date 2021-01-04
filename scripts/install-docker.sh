7e333d8e06a1da3b7c929de018b8cc7f1e092cba
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2021-01-04 19:49:46 +0100

    Pin Docker versions

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-apt-get update
+VERSION_DOCKER="20.10.1"
+VERSION_COMPOSE="1.27.4"
+
+apt-get update -y
 apt-get install -y curl
 curl -fsSL https://get.docker.com -o get-docker.sh
+export VERSION=${VERSION_DOCKER}
 sh get-docker.sh
 usermod -aG docker vagrant
-curl -L https://github.com/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/${VERSION_COMPOSE}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+docker-compose --version
